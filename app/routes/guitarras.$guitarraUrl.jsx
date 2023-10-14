adc6a17b6505be2194f5960329dcc6c4e81fc3cd
arrevillagamario <marrevillaga03@gmail.com>
2023-10-14 13:29:53 -0600

    Ya hay routing dinamico en las guitarras

--- app/routes/guitarras.$guitarraUrl.jsx
+++ app/routes/guitarras.$guitarraUrl.jsx
@@ -1,15 +1,51 @@
 import React from "react";
 import { getGuitarra } from "../models/guitarras.server.js";
 import { useLoaderData } from "@remix-run/react";
+import styles from "../styles/guitarras.css";
 
 export async function loader(req) {
-  const guitarra = await getGuitarra(req.params.guitarraUrl);
-  console.log(guitarra);
-  return {};
+  const guitarraUrl = req.params.guitarraUrl;
+  const guitarra = await getGuitarra(guitarraUrl);
+
+  return guitarra;
+}
+
+export function meta({ data }) {
+  const { nombre } = data.data[0].attributes;
+  return [
+    {
+      title: `GuitarLA - ${nombre}`,
+      description: `Guitarras, Venta de guitarras, Guitarra ${nombre}`,
+    },
+  ];
+}
+
+export function links() {
+  return [
+    {
+      rel: "stylesheet",
+      href: styles,
+    },
+  ];
 }
 const Guitarra = () => {
-  const guitarra = loader();
-  return <div>Guitarra</div>;
+  const guitarra = useLoaderData();
+  const { nombre, descripcion, precio, imagen } = guitarra.data[0].attributes;
+  return (
+    <main className="contenedor guitarra">
+      <img
+        src={imagen.data.attributes.url}
+        alt={`Imagen de guitarra ${nombre}`}
+        className="imagen"
+      />
+
+      <div className="contenido">
+        <h3>{nombre}</h3>
+        <p className="texto">{descripcion}</p>
+        <p className="precio">${precio}</p>
+      </div>
+    </main>
+  );
 };
 
 export default Guitarra;
